ebf76f0bf2f0f254d6446767395807d6533a8ede
Shaun Limbeek <soccercomet@yahoo.com>
2020-12-17 16:07:09 -0800

    defined in arrays and key input variables

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Create variables
 var passwordLength = 0;
-var password ="";
+var passwordOutput ="";
 var incLower = false;
 var incUpper = false;
 var incNumber = false;
 var incSpecial = false;
 
-
-
+// Create arrays
+var LowerArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var UpperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var NumberArray = [0,1,2,3,4,5,6,7,8,9];
+var SpecialArray = ["!","@","#","$","%","^","&","*","(",")"];
 
 
 // Write password to the #password input
